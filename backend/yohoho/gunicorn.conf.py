6bc466dad7e4f1eb2e3d5dbe377d893f6320d255
yujin <root@yujin.io>
2023-09-25 18:33:37 +0200

    make gunicorn config independent

--- backend/yohoho/gunicorn.conf.py
+++ backend/yohoho/gunicorn.conf.py
@@ -1,7 +1,12 @@
-from . import config
+import multiprocessing as mp
 
-host = config.GUNICORN_HOST
-port = config.GUNICORN_PORT
+from environs import Env
+
+env = Env()
+env.read_env()
+
+host = env.str("GUNICORN_HOST", "0.0.0.0")
+port = env.int("GUNICORN_PORT", 5000)
 bind = f"{host}:{port}"
-workers = config.GUNICORN_WORKERS
-worker_class = config.GUNICORN_WORKER_CLASS
+workers = env.int("GUNICORN_WORKERS", mp.cpu_count() * 2 + 1)
+worker_class = "uvicorn.workers.UvicornWorker"
