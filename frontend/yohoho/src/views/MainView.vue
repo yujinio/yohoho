7e1837ad40bcb238b52f932750a4cfa723e5d930
yujin <root@yujin.io>
2023-09-28 01:21:06 +0200

    add button to id search

--- frontend/yohoho/src/views/MainView.vue
+++ frontend/yohoho/src/views/MainView.vue
@@ -38,13 +38,19 @@
                   />
                 </n-tab-pane>
                 <n-tab-pane name="По ID на Кинопоиске">
-                  <n-input
-                    :loading="getMovieByKinopoiskIdDelayedLoading"
-                    size="large"
-                    type="text"
-                    placeholder="12345"
-                    @update:value="getMovieByKinopoiskIdDelayed"
-                  />
+                  <n-grid cols="7" item-responsive responsive="screen" x-gap="24" y-gap="24">
+                    <n-gi span="7 m:6 l:5 xl:6">
+                      <n-input
+                        size="large"
+                        type="text"
+                        placeholder="12345"
+                        v-model:value="movieKinopoiskId"
+                      />
+                    </n-gi>
+                    <n-gi span="3 m:1 l:1 xl:1" offset="3 m:0 l:0 xl:0">
+                      <n-button size="large" @click="getMovieByKinopoiskIdClick">Поиск</n-button>
+                    </n-gi>
+                  </n-grid>
                 </n-tab-pane>
               </n-tabs>
             </n-card>
@@ -56,31 +62,31 @@
 </template>
 
 <script setup lang="ts">
+import axios from 'axios'
 import {
-  NLayout,
   NAutoComplete,
+  NButton,
   NCard,
-  NLayoutContent,
-  NGrid,
   NGi,
+  NGrid,
+  NInput,
+  NLayout,
+  NLayoutContent,
   NTabPane,
   NTabs,
-  NInput,
-  useMessage,
-  useLoadingBar
+  useLoadingBar,
+  useMessage
 } from 'naive-ui'
 import { ref } from 'vue'
-import axios from 'axios'
 
 const BASE_BACKEND_SERVER_URL = import.meta.env.VITE_BASE_API_SERVER_URL
 
 const iframes = ref([])
-const getMovieByKinopoiskIdTimeout = ref(0)
 const movieSearchTimeout = ref(0)
 const movieSearchValue = ref('')
 const movieSearchOptions = ref([])
 const movieSearchLoading = ref(false)
-const getMovieByKinopoiskIdDelayedLoading = ref(false)
+const movieKinopoiskId = ref('')
 const movieLoadingBar = useLoadingBar()
 const message = useMessage()
 
@@ -90,6 +96,10 @@ const resetMovieSearch = () => {
   movieSearchValue.value = ''
   movieSearchOptions.value = []
   movieSearchLoading.value = false
+}
+
+const getMovieByKinopoiskIdClick = async () => {
+  return await getMovieByKinopoiskId(movieKinopoiskId.value)
 }
 
 const getMovieByKinopoiskId = async (kinopoiskId: string | number) => {
@@ -115,16 +125,6 @@ const getMovieByKinopoiskId = async (kinopoiskId: string | number) => {
     })
     movieLoadingBar.error()
   }
-}
-
-const getMovieByKinopoiskIdDelayed = async (kinopoiskId: string | number) => {
-  if (getMovieByKinopoiskIdTimeout.value) clearTimeout(getMovieByKinopoiskIdTimeout.value)
-  if (!getMovieByKinopoiskIdDelayedLoading.value) getMovieByKinopoiskIdDelayedLoading.value = true
-
-  getMovieByKinopoiskIdTimeout.value = setTimeout(async () => {
-    await getMovieByKinopoiskId(kinopoiskId)
-    getMovieByKinopoiskIdDelayedLoading.value = false
-  }, 2500)
 }
 
 const updateMovieSearchOptions = async (value: string) => {
